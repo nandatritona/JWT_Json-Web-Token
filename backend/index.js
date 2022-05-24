6689fd4d5864ebc9b52ec153658b58ea0cb84eef
unknown <user@ITD35.ITD35>
2022-05-24 15:36:14 +0700

    Berhasil membuat table dengan js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import Express from "express";
 import db from "./config/Database.js";
+import Users from "./models/UserModel.js";
+
 const app = Express();
 
 try {
     await db.authenticate();
     console.log("Connection has been successfully.");
+    await Users.sync(); // jika tdk ada tbl users di db, maka akan di buat kan secara auto
+
 } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error(error);
 }
 
 app.listen(5000, () => console.log("Server started on port 5000"));
