2a63d5e7af350f54ed38ab83e30823a2609d114d
unknown <user@ITD35.ITD35>
2022-05-24 15:43:42 +0700

    berhasil conect ke db

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 import Express from "express";
 import db from "./config/Database.js";
-import Users from "./models/UserModel.js";
+// import Users from "./models/UserModel.js";
 
 const app = Express();
 
 try {
     await db.authenticate();
     console.log("Connection has been successfully.");
-    await Users.sync(); // jika tdk ada tbl users di db, maka akan di buat kan secara auto
-
+    // jika tdk ada tbl users di db, maka akan di buat kan secara auto
+    // await Users.sync();
 } catch (error) {
     console.error(error);
 }
