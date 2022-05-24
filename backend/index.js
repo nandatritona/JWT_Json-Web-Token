1fd4082b2f24e1bce209a963a409a2f850e1306c
unknown <user@ITD35.ITD35>
2022-05-24 15:16:39 +0700

    Menguji db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 import Express from "express";
+import db from "./config/Database.js";
 const app = Express();
+
+try {
+    await db.authenticate();
+    console.log("Connection has been successfully.");
+} catch (error) {
+    console.error("Unable to connect to the database:", error);
+}
 
 app.listen(5000, () => console.log("Server started on port 5000"));
