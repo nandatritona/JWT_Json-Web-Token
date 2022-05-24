e15b4c40379cfcc538d46df231dc12006e19fe76
unknown <user@ITD35.ITD35>
2022-05-24 16:59:11 +0700

    Berhasil terhubung ke API serve localhost:5000

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,4 +1,5 @@
 import Users from "../models/UserModel.js";
+import bcrypt from "bcrypt";
 
 export const getUsers = async (req, res) => {
     try {
@@ -8,5 +9,25 @@ export const getUsers = async (req, res) => {
     } catch (error) {
         console.log(error);
         // res.status(500).send(error);
+    }
+}
+
+export const Register = async (req, res) => {
+    const { name, email, password, confPassword } = req.body;
+    // if (password !== confPassword) return res.status(400).json({ msg: "Password doesn't match" });
+    if (password !== confPassword) {
+        return res.status(400).json("Password doesn't match");
+    }
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+    try {
+        const user = await Users.create({
+            name: name,
+            email: email,
+            password: hashPassword,
+        });
+        res.json("Register Success");
+    } catch (error) {
+        console.log(error);
     }
 }
